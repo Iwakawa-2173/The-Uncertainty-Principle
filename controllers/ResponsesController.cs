e197a96f616f3ac6999a24343462eb4dbfb35c86
岩川温子と真光 <164639908+Iwakawa-2173@users.noreply.github.com>
2024-10-12 23:52:34 +0300

    Update ResponsesController.cs

--- controllers/ResponsesController.cs
+++ controllers/ResponsesController.cs
@@ -6,54 +6,54 @@ namespace The_Uncertainty_Principle.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ResponsesController : ControllerBase
+    public class ResponsesController : ControllerBase, IResponsesController // Реализация интерфейса
     {
-        private readonly GameDbContext _context;
+      private readonly GameDbContext _context;
 
-        public ResponsesController(GameDbContext context)
-        {
-            _context = context;
-        }
+      public ResponsesController(GameDbContext context)
+      {
+          _context = context;
+      }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<Response>> GetResponses()
-        {
-            return _context.Responses.ToList();
-        }
+      [HttpGet]
+      public ActionResult<IEnumerable<Response>> GetResponses()
+      {
+          return _context.Responses.ToList();
+      }
 
-        [HttpGet("{id}")]
-        public ActionResult<Response> GetResponse(int id)
-        {
-            var response = _context.Responses.Find(id);
-            if (response == null) return BadRequest();
-            return response;
-        }
+      [HttpGet("{id}")]
+      public ActionResult<Response> GetResponse(int id)
+      {
+          var response = _context.Responses.Find(id);
+          if (response == null) return BadRequest();
+          return response;
+      }
 
-        [HttpPost]
-        public ActionResult<Response> CreateResponse(Response response)
-        {
-            _context.Responses.Add(response);
-            _context.SaveChanges();
-            return CreatedAtAction(nameof(GetResponse), new { id = response.ResponseId }, response);
-        }
+      [HttpPost]
+      public ActionResult<Response> CreateResponse(Response response)
+      {
+          _context.Responses.Add(response);
+          _context.SaveChanges();
+          return CreatedAtAction(nameof(GetResponse), new { id = response.ResponseId }, response);
+      }
 
-        [HttpPut("{id}")]
-        public IActionResult UpdateResponse(int id, Response response)
-        {
-            if (id != response.ResponseId) return BadRequest();
-            _context.Entry(response).State = EntityState.Modified;
-            _context.SaveChanges();
-            return NoContent();
-        }
+      [HttpPut("{id}")]
+      public IActionResult UpdateResponse(int id, Response response)
+      {
+          if (id != response.ResponseId) return BadRequest();
+          _context.Entry(response).State = EntityState.Modified;
+          _context.SaveChanges();
+          return NoContent();
+      }
 
-        [HttpDelete("{id}")]
-        public IActionResult DeleteResponse(int id)
-        {
-            var response = _context.Responses.Find(id);
-            if (response == null) return BadRequest();
-            _context.Responses.Remove(response);
-            _context.SaveChanges();
-            return NoContent();
-        }
-    }
+      [HttpDelete("{id}")]
+      public IActionResult DeleteResponse(int id)
+      {
+          var response = _context.Responses.Find(id);
+          if (response == null) return BadRequest();
+          _context.Responses.Remove(response);
+          _context.SaveChanges();
+          return NoContent();
+      }
+   }
 }
