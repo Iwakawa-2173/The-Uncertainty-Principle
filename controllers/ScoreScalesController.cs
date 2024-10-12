3cd7a8fdff97a386d964a3bc1876607181336fd3
岩川温子と真光 <164639908+Iwakawa-2173@users.noreply.github.com>
2024-10-12 23:52:55 +0300

    Update ScoreScalesController.cs

--- controllers/ScoreScalesController.cs
+++ controllers/ScoreScalesController.cs
@@ -4,56 +4,56 @@ using System.Linq;
 
 namespace The_Uncertainty_Principle.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ScoreScalesController : ControllerBase
-    {
-       private readonly GameDbContext _context;
+   [Route("api/[controller]")]
+   [ApiController]
+   public class ScoreScalesController : ControllerBase, IScoreScalesController // Реализация интерфейса
+   {
+      private readonly GameDbContext _context;
 
-       public ScoreScalesController(GameDbContext context)
-       {
-           _context = context;
-       }
+      public ScoreScalesController(GameDbContext context)
+      {
+          _context = context;
+      }
 
-       [HttpGet]
-       public ActionResult<IEnumerable<ScoreScale>> GetScoreScales()
-       {
-           return _context.ScoreScales.ToList();
-       }
+      [HttpGet]
+      public ActionResult<IEnumerable<ScoreScale>> GetScoreScales()
+      {
+          return _context.ScoreScales.ToList();
+      }
 
-       [HttpGet("{id}")]
-       public ActionResult<ScoreScale> GetScoreScale(int id)
-       {
-           var scoreScale = _context.ScoreScales.Find(id);
-           if (scoreScale == null) return BadRequest();
-           return scoreScale;
-       }
+      [HttpGet("{id}")]
+      public ActionResult<ScoreScale> GetScoreScale(int id)
+      {
+          var scoreScale = _context.ScoreScales.Find(id);
+          if (scoreScale == null) return BadRequest();
+          return scoreScale;
+      }
 
-       [HttpPost]
-       public ActionResult<ScoreScale> CreateScoreScale(ScoreScale scoreScale)
-       {
-           _context.ScoreScales.Add(scoreScale);
-           _context.SaveChanges();
-           return CreatedAtAction(nameof(GetScoreScale), new { id = scoreScale.ScaleId }, scoreScale);
-       }
+      [HttpPost]
+      public ActionResult<ScoreScale> CreateScoreScale(ScoreScale scoreScale)
+      {
+          _context.ScoreScales.Add(scoreScale);
+          _context.SaveChanges();
+          return CreatedAtAction(nameof(GetScoreScale), new { id = scoreScale.ScaleId }, scoreScale);
+      }
 
-       [HttpPut("{id}")]
-       public IActionResult UpdateScoreScale(int id, ScoreScale scoreScale)
-       {
-           if (id != scoreScale.ScaleId) return BadRequest();
-           _context.Entry(scoreScale).State = EntityState.Modified;
-           _context.SaveChanges();
-           return NoContent();
-       }
+      [HttpPut("{id}")]
+      public IActionResult UpdateScoreScale(int id, ScoreScale scoreScale)
+      {
+          if (id != scoreScale.ScaleId) return BadRequest();
+          _context.Entry(scoreScale).State = EntityState.Modified;
+          _context.SaveChanges();
+          return NoContent();
+      }
 
-       [HttpDelete("{id}")]
-       public IActionResult DeleteScoreScale(int id)
-       {
-           var scoreScale = _context.ScoreScales.Find(id);
-           if (scoreScale == null) return BadRequest();
-           _context.ScoreScales.Remove(scoreScale);
-           _context.SaveChanges();
-           return NoContent();
-       }
+      [HttpDelete("{id}")]
+      public IActionResult DeleteScoreScale(int id)
+      {
+          var scoreScale = _context.ScoreScales.Find(id);
+          if (scoreScale == null) return BadRequest();
+          _context.ScoreScales.Remove(scoreScale);
+          _context.SaveChanges();
+          return NoContent();
+      }
    }
 }
