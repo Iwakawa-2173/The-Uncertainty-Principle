8c4c6854b8ac8403b9a3c1b80c4be8ffdb5978e5
岩川温子 <164639908+Iwakawa-2173@users.noreply.github.com>
2024-11-22 16:07:50 +0300

    Delete The-Uncertainty-Principle directory

--- The-Uncertainty-Principle/src/TUP.WebApi/TUP.WebApi/Domain/Entities/Event.cs
+++
@@ -1,9 +0,0 @@
-namespace TUP.WebApi.Domain.Entities;
-
-public class Event
-{
-    public int EventId { get; set; }
-    public int EventNumber { get; set; }
-    public bool IsSignificant { get; set; }
-    public string Description { get; set; }
-}
--- The-Uncertainty-Principle/src/TUP.WebApi/TUP.WebApi/Domain/Entities/Player.cs
+++
@@ -1,8 +0,0 @@
-namespace TUP.WebApi.Domain.Entities;
-
-public class Player
-{
-    public int PlayerId { get; set; }
-    public string UniquePlayerName { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
